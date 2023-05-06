46624c3af64595371eefda4a8ca848e795c98ea3
Hussnain Ahmad <hussnainahmad1606@gmail.com>
2023-05-06 21:31:48 +0500

    Add firebase storage to store images

---
+++ firebase/firebaseStorage.js
@@ -0,0 +1,21 @@
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: "gameverse-f352c.appspot.com",
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
+};
+
+
+
+const app = initializeApp(firebaseConfig);
+
+
+export const storage = getStorage();
